1c15d0543c78398192c47b0d76c5eaeda30a2586
tumee <tmkee0525@gmail.com>
2024-10-16 00:24:58 +0900

    add to backbutton

--- app/Filament/Resources/OrderResource/Pages/ViewOrder.php
+++ app/Filament/Resources/OrderResource/Pages/ViewOrder.php
@@ -4,9 +4,22 @@ namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewOrder extends ViewRecord
 {
     protected static string $resource = OrderResource::class;
+    
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('back')
+                ->url(OrderResource::getUrl()) 
+                ->button()
+                ->color('gray'),
+            Actions\EditAction::make(),
+        ];
+    }
+    
 }
--- app/Filament/Resources/ProjectResource/Pages/ViewProject.php
+++ app/Filament/Resources/ProjectResource/Pages/ViewProject.php
@@ -4,9 +4,21 @@ namespace App\Filament\Resources\ProjectResource\Pages;
 
 use App\Filament\Resources\ProjectResource;
 use Filament\Actions;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewProject extends ViewRecord
 {
     protected static string $resource = ProjectResource::class;
+    
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('back')
+                ->url(ProjectResource::getUrl())
+                ->button()
+                ->color('gray'),
+            Actions\EditAction::make(),
+        ];
+    }
 }
